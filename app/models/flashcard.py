963a3e69c9450fbb68386289e9c6594fd2d9ef67
cmizell186 <chrislmiz311@gmail.com>
2022-05-11 08:10:55 -0700

    created flashcard  model

--- app/models/flashcard.py
+++ app/models/flashcard.py
@@ -1,22 +1,22 @@
 from .db import db
 from datetime import datetime
 
-class Quiz(db.Model):
-    __tablename__ = 'quizzes'
+class FlashCard(db.Model):
+    __tablename__ = 'flashcards'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False) # references user id
-    subject_id = db.Column(db.Integer, nullable=False) # references subject id
-    title = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    quiz_id = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def to_dict(self):
         return{
-            "id": self.id,
+            'id': self.id,
             "user_id": self.user_id,
-            "subject_id": self.subject_id,
+            'quiz_id': self.quiz_id,
+            'description': self.description,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
         }
