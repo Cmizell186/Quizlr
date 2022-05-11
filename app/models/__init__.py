1aa684747066307f6af8372fbe11455e85021492
cmizell186 <chrislmiz311@gmail.com>
2022-05-11 08:45:58 -0700

    created study set models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
 from .subject import Subject
+from .quiz import Quiz
+from .flashcard import FlashCard
+from .studyset import StudySet
+from.studysetquiz import StudySetQuiz
---
+++ app/models/studysetquiz.py
@@ -0,0 +1,20 @@
+from .db import db
+from datetime import datetime
+
+class StudySetQuiz(db.Model):
+    __tablename__ = 'studysetquizzes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, nullable=False)
+    study_set_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'user_id': self.quiz_id,
+            'study_set_id': self.study_set_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
