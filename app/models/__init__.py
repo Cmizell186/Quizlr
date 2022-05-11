74e677c3e2c2243e0ff24f05f183d9117bdf4f08
cmizell186 <chrislmiz311@gmail.com>
2022-05-11 09:49:56 -0700

    created migration for new tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,4 @@ from .subject import Subject
 from .quiz import Quiz
 from .flashcard import FlashCard
 from .studyset import StudySet
-from.studysetquiz import StudySetQuiz
+from .studysetquiz import StudySetQuiz
