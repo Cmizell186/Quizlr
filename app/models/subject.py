0dc299c366843ffd8fbe01b6a34018378b715b91
cmizell186 <chrislmiz311@gmail.com>
2022-05-11 08:47:57 -0700

    updated subject model

--- app/models/subject.py
+++ app/models/subject.py
@@ -5,10 +5,12 @@ class Subject(db.Model):
     __tablename__ = 'subjects'
 
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
     subject = db.Column(db.String, nullable=False, unique=True)
 
     def to_dict(self):
         return{
             'id': self.id,
+            'title': self.title,
             'subject': self.subject,
         }
