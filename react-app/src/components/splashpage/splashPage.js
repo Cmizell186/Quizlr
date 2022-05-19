29e4809cca4eebad3d9ec2acf45d5ba370360bf0
cmizell186 <chrislmiz311@gmail.com>
2022-05-18 17:24:21 -0700

    finished splash page

--- react-app/src/components/splashpage/splashPage.js
+++ react-app/src/components/splashpage/splashPage.js
@@ -18,6 +18,40 @@ const SplashPage = () =>{
                     Start creating quizzes and flash cards now.
                 </p>
             </div>
+
+            <div className="splash-page-content">
+                <div className="info-splash-area">
+                    <p className="students-who-use">
+                        <em id="id90em">90%</em>
+                        of the students who use Quizlr report higher grades.
+                    </p>
+                </div>
+                <div className="flashcards-on-area">
+                    <div className="flashcards-image">
+                        <img height="400" src="https://images.prismic.io/quizlet-prod/d4052d90-f71e-466a-86f5-080cf02de2da_20210814_QZ_Home_Flashcards.png?auto=compress,format&rect=0,2,3072,2395&w=1026&h=800 1x, https://images.prismic.io/quizlet-prod/d4052d90-f71e-466a-86f5-080cf02de2da_20210814_QZ_Home_Flashcards.png?auto=compress,format&rect=0,2,3072,2395&w=1026&h=800 2x"/>
+                    </div>
+                    <div className="flashcards-splash-desc">
+                        <h2 id="flashcard-info-h2">Flashcards on repeat. Study modes on shuffle.</h2>
+                        <p id="flashcard-info-p">Mixed with smart study tools, our flashcards have been helping students ace their toughest exams since 2005.</p>
+                    </div>
+                </div>
+                <div className="jam-on-area">
+                    <div className="jam-image">
+                        <img height="400" width="514" src="https://onlysounds.s3.us-west-1.amazonaws.com/razer-nommo-chroma.png"/>
+                    </div>
+                    <div className="jam-splash-desc">
+                        <h2 id="flashcard-info-h2">Whether you plan or cram, find your study jam.</h2>
+                        <p id="flashcard-info-p">Early morning? All-nighter? With teaching methods backed by learning science, Quizlet is designed to save you time.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom-splash-page">
+                <div className="my-links">
+                    <p>Created by Christopher Mizell</p>
+                    <a href="https://github.com/Cmizell186">Github</a>
+                    <a href="https://www.linkedin.com/in/christopher-mizell-4b21a4174/">LinkedIn</a>
+                </div>
+            </div>
         </>
     )
 }
