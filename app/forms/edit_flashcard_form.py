cd0d710e326469d290a08df46eebb9ad7accbfd7
cmizell186 <chrislmiz311@gmail.com>
2022-05-18 21:11:13 -0700

    added more validators

--- app/forms/edit_flashcard_form.py
+++ app/forms/edit_flashcard_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from app.models import FlashCard
 
 class UpdateFlashcardForm(FlaskForm):
-    front = StringField('front', validators=[DataRequired(message="Please provide title")])
-    back = StringField('back', validators=[DataRequired(message="Please provide answer")])
+    front = StringField('front', validators=[DataRequired(message="Please provide title"),
+    Length(max=150, message="Question exceeds 150 character limit")])
+    back = StringField('back', validators=[DataRequired(message="Please provide answer"),
+    Length(max=150, message="Answer exceeds 150 character limit")])
--- app/forms/flashcard_form.py
+++ app/forms/flashcard_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from app.models import FlashCard
 
 class NewFlashcardForm(FlaskForm):
-    front = StringField('front', validators=[DataRequired(message="Please provide question")])
-    back = StringField('back', validators=[DataRequired(message="Please provide answer")])
+    front = StringField('front', validators=[DataRequired(message="Please provide question"),
+    Length(max=150, message="Question exceeds 150 character limit")])
+    back = StringField('back', validators=[DataRequired(message="Please provide answer"),
+    Length(max=150, message="Answer exceeds 150 character limit")])
