7af6833f7e695fb401a09ac6b75e2a0864fa68f8
cmizell186 <chrislmiz311@gmail.com>
2023-05-01 12:54:05 -0700

    possible fix to dead images

--- react-app/src/components/splashpage/splashPage.js
+++ react-app/src/components/splashpage/splashPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./splashPage.css";
+// import "../../public/static/images"
+import "react-app/public/static/images";
 
 
 const SplashPage = () =>{
     return (
         <>
             <div className="chris-image-container">
-                <img  className="splash-image" src="https://onlysounds.s3.us-west-1.amazonaws.com/chris-splash-photo.png"/>
+                <img  className="splash-image" src="react-app/public/static/images/chris-splash-photo.png"/>
             </div>
             <div className="text-splash-area">
                 <h1 style={{color: 'white', fontSize:'32px'}}>
@@ -37,7 +39,7 @@ const SplashPage = () =>{
                 </div>
                 <div className="jam-on-area">
                     <div className="jam-image">
-                        <img height="400" width="514" src="https://onlysounds.s3.us-west-1.amazonaws.com/razer-nommo-chroma.png"/>
+                        <img height="400" width="514" src="/home/chris/chris-captstone-project/react-app/public/static/images/razer-nommo-chroma.pngrazer-nommo-chroma"/>
                     </div>
                     <div className="jam-splash-desc">
                         <h2 id="flashcard-info-h2">Whether you plan or cram, find your study jam.</h2>
